0fd3557af06368b9447b2cc444052824e99c288e
Sourav kumar <161188554+sourav842741@users.noreply.github.com>
2025-09-21 22:41:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-   origin:"http://localhost:5173",
+   origin:"https://open-tube-1.onrender.com",
    credentials:true
 }))
 
